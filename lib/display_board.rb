0aa6f8e75e96ded77059e4170125f66b6e9d324d
Alethea Cary <aletheacary@gmail.com>
2019-11-13 12:42:52 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,15 @@
 # Define display_board that accepts a board and prints
 def display_board (board)
   rows = []
-
   rows << " #{board[0]} | #{board[1]} | #{board[2]} "
   rows << "-----------"
   rows << " #{board[3]} | #{board[4]} | #{board[5]} "
   rows << "-----------"
   rows << " #{board[6]} | #{board[7]} | #{board[8]} "
+
+  rows.each do |row|
+    puts row
+  end
 
 
 
